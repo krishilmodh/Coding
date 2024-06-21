e72e7ea27470c0e857d51cb92f0fe0c75b7bd1f4
krishil modh <78547459+krishilmodh@users.noreply.github.com>
2024-06-21 22:54:12 +0530

    5 10 june (#9)
    * second largest number
    
    * feat: Add bubble sort, word count, and remove duplicate functions
    
    * chore: Refactor bubble sort algorithm
    
    ---------
    
    Co-authored-by: krishimodh <krishimodh@gmail.com>

--- Beginner/09_bubble_sort.py
+++ Beginner/09_bubble_sort.py
@@ -1,7 +1,9 @@
 def BubbleSort(nums):
     n = len(nums)
     for i in range(n):
-        for j in range(n - 1):
+
+        for j in range(i + 1, n):
+
             temp = 0
             if nums[i] < nums[j]:
                 temp = nums[j]
